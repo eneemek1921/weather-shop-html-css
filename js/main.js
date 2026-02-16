7d34c3d71a97d0b5bb9d6673d33a2933f2afb16f
Eneemek1921 <Eneemek1921@gmail.com>
2026-02-16 23:08:11 +0300

    Send a weather-card's to site, used API and ForEach

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 console.log('js work')
 
+let dataWeather = []
+
 document.addEventListener('DOMContentLoaded', ()=>{
     async function loadPage(pageName) {
         const response = await fetch(`../data/pages/${pageName}.html`)
         const html = await response.text();
         const container = document.querySelector('.container')
         container.innerHTML = html;
-        const mainBtn = document.querySelector('.mainBtn')
+        const mainBtn = document.querySelector('#mainBtn')
         if (mainBtn) {
             mainBtn.addEventListener('click', ()=>{
                 loadPage('main')
             })
         }
-        const contactsBtn = document.querySelector('.contactsBtn')
-        if (mainBtn) {
+        const contactsBtn = document.querySelector('#contactsBtn')
+        if (contactsBtn) {
             contactsBtn.addEventListener('click', ()=>{
                 loadPage('contacts')
             })
+        }
+        if (pageName === 'main') {
+            initWeather();
         }
     }
 
@@ -24,6 +29,38 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 async function initWeather() {
-    const weather = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.5244&longitude=13.4105&hourly=temperature_2m,precipitation_probability,wind_speed_10m,apparent_temperature')
-    const response = weather.JSON
+    try {
+        const api = ('https://api.open-meteo.com/v1/forecast?latitude=52.5244&longitude=13.4105&hourly=temperature_2m,precipitation_probability,wind_speed_10m,apparent_temperature')
+        const weather = await fetch(api)
+        const response = await weather.json();
+        dataWeather = response;
+        const container = document.querySelector('.weather-content')
+        container.innerHTML = ''
+
+        dataWeather.hourly.time.forEach((time, index) => {
+            const formatedTime = new Date(time).toLocaleString('en-EN', {
+                day: 'numeric',
+                month: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
+            })
+            const temp = dataWeather.hourly.temperature_2m[index]
+            const wind = dataWeather.hourly.wind_speed_10m[index]
+            const precip = dataWeather.hourly.precipitation_probability[index]
+            const feelsLike = dataWeather.hourly.apparent_temperature[index]
+            const weatherCard = document.createElement('div');
+            weatherCard.className = 'weatherCard';
+            weatherCard.innerHTML = `
+                <span>Time: ${formatedTime}</span>
+                <span>Temperature: ${temp}</span>
+                <span>Wind: ${wind}</span>
+                <span>Precip: ${precip}</span>
+                <span>feelsLike: ${feelsLike}</span>
+            `;
+            container.appendChild(weatherCard)
+        });
+    } catch (error) {
+        console.log('error - initWeather', error)
+    }
+
 }
